705b3fd338100efbd74dd928256c168055a52a52
Daniel Chavez <daniel.chavez.epet20@gmail.com>
2022-11-23 13:44:19 -0300

    Componente LogoIcon en SVG creado e importado. Algunos estilos mas al layout

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,22 @@
 import React from "react";
 import CartWidget from "../CartWidget/CartWidget";
+import LogoIcon from "../LogoIcon/LogoIcon";
 import NavCategory from "../NavCategory/NavCategory";
 
 export default function Navbar() {
   return (
-    <nav className="w-full h-24 bg-neutral-100 shadow-lg flex items-center justify-between px-10">
-      <div className="flex flex-row gap-16 items-center">
-        <h1 className="text-3xl font-bold cursor-default">Poly.mart</h1>
+    <nav className="w-full h-24 bg-white shadow-lg flex items-center justify-between px-10">
+      <section className="flex flex-row gap-24 items-center">
+        <div className="flex items-center gap-8">
+          <LogoIcon />
+          <h1 className="text-3xl font-bold cursor-default">Poly.mart</h1>
+        </div>
         <div className="flex flex-row gap-4 items-center">
           <NavCategory title="Low-poly assets" />
           <NavCategory title="Game assets" />
           <NavCategory title="High-poly assets" />
         </div>
-      </div>
+      </section>
       <CartWidget />
     </nav>
   );
